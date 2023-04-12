becfb0e80c956e216661499cc744a8c6f4a069e8
João vitor <joaoviitor888@outlook.com>
2023-04-12 00:29:27 -0300

    função para finalizar a tarefa

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,44 @@
-import React, { useState } from "react";
+import { useState } from "react";
+import "./App.css";
+import CardAdicionar from "./components/CardAdicionar";
+import ListItem from "./components/ListItem";
 
 function App() {
-  const [tasks, setTasks] = useState([]);
+  const [listaDeTarefas, setListaDeTarefas] = useState([
+    { id: 1, texto: "Tarefa 1", finalizado: false },
+  ]);
+
+  function adicionarTarefa(tarefa) {
+    debugger;
+    const novaTarefa = {
+      id: listaDeTarefas.length + 1,
+      texto: tarefa,
+      finalizado: false,
+    };
+
+    const novaListaGeral = [...listaDeTarefas];
+    novaListaGeral.push(novaTarefa);
+
+    setListaDeTarefas(novaListaGeral);
+  }
+
+  function removerTarefa(tarefaParaRemover) {
+    const novaListaGeral = listaDeTarefas.filter(
+      (tarefa) => tarefa.id !== tarefaParaRemover.id
+    );
+    setListaDeTarefas(novaListaGeral);
+  }
+
+  return (
+    <div className="App">
+      <CardAdicionar adicionarTarefa={adicionarTarefa} />
+      <ul>
+        {listaDeTarefas.map((tarefa) => (
+          <ListItem key={tarefa.id} tarefa={tarefa} />
+        ))}
+      </ul>
+    </div>
+  );
 }
-
-const addTask = (taskText) => {
-  const newTask = {
-    id: Date.now(),
-    text: taskText,
-    completed: false,
-  };
-  setTasks([...tasks, newTask]);
-};
-
-return (
-  <div>
-    <AddTaskForm addTask={addTask} />
-    {/* Restante do código */}
-  </div>
-);
 
 export default App;
